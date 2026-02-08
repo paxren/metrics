7b25fdd790b76fc78492925beee0e0018c1590e8
userg <you@example.com>
2026-02-08 20:37:25 +0500

    test

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -8,13 +8,38 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/gordonklaus/ineffassign/pkg/ineffassign"
 	"github.com/paxren/metrics/cmd/staticlint/osexit"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/ifaceassert"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/reflectvaluecompare"
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/slog"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/stringintconv"
 	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
 	"honnef.co/go/tools/staticcheck"
 )
 
@@ -176,9 +201,38 @@ func main() {
 	}
 	mychecks := []*analysis.Analyzer{
 		ErrCheckAnalyzer,
+		// Стандартные анализаторы из golang.org/x/tools/go/analysis/passes
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		copylock.Analyzer,
+		deepequalerrors.Analyzer,
+		errorsas.Analyzer,
+		httpresponse.Analyzer,
+		ifaceassert.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
 		printf.Analyzer,
+		reflectvaluecompare.Analyzer,
 		shadow.Analyzer,
+		shift.Analyzer,
+		slog.Analyzer,
+		stdmethods.Analyzer,
+		stringintconv.Analyzer,
 		structtag.Analyzer,
+		testinggoroutine.Analyzer,
+		tests.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
+		// Публичные анализаторы
+		ineffassign.Analyzer,
+		// Пользовательские анализаторы
 		osexit.Analyzer,
 	}
 	checks := make(map[string]bool)
@@ -191,12 +245,5 @@ func main() {
 			mychecks = append(mychecks, v.Analyzer)
 		}
 	}
-	multichecker.Main(
-		//ErrCheckAnalyzer,
-		printf.Analyzer,
-		shadow.Analyzer,
-		shift.Analyzer,
-		structtag.Analyzer,
-		osexit.Analyzer,
-	)
+	multichecker.Main(mychecks...)
 }
